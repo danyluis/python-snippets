0bd2f47d8d14fd51e94c2c9a43a808d60cc8bf4c
Dany Fariña <dany.farina@glovoapp.com>
2022-02-07 22:06:19 +0100

    Add util to sort JSON files. Mainly useful for comparisons

---
+++ util/sort-json.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import json
+import sys
+from collections import OrderedDict
+
+DICT_DELIM = '='
+LIST_DELIM = ','
+
+def read_all():
+	return "".join(sys.stdin.readlines())
+
+def to_str(x):
+	if isinstance(x, str):
+		return x
+
+	if type(x) in {int, float}:
+		return str(x)
+
+	if isinstance(x, dict):
+		sorted_keys = sorted("(" + to_str(k) + DICT_DELIM + to_str(x[k]) + ")" for k in x.keys())
+		return LIST_DELIM.join(sorted_keys)
+
+	if isinstance(x, list):
+		sorted_values = sorted([to_str(i) for i in x])
+		return  "(" + LIST_DELIM.join(sorted_values) + ")"
+
+	return "null"
+
+
+def sort_obj(obj):
+	if isinstance(obj, list):
+		return sorted([sort_obj(i) for i in obj], key=to_str)
+
+	if isinstance(obj, dict):
+		sorted_keys = sorted(obj.keys())
+		return OrderedDict((k, sort_obj(obj[k])) for k in sorted_keys)
+
+	return obj
+
+
+print(json.dumps(sort_obj(json.loads(read_all()))))
